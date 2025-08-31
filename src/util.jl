75376b8a9d4b1f320379eda1f26a514c0b601ce5
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-31 00:54:17 -0300

    Update

--- src/util.jl
+++ src/util.jl
@@ -1,5 +1,10 @@
 Optional{T} = Union{T, Nothing}
 
 function escape_json(str::String)
-    return replace(replace(replace(str, "\\" => "\\\\"), "\"" => "\\\""), "\n" => "\\n")
+    return replace(
+        replace(
+            replace(str, "\\" => "\\\\"),
+            "\"" => "\\\""),
+        "\n" => "\\n",
+    )
 end
