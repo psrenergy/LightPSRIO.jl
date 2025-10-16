4d98d8d9c2d9b721339db237c8937a2503503b31
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-10-16 16:40:14 -0300

    Update

--- test/expressions/unary/test_profile.jl
+++ test/expressions/unary/test_profile.jl
@@ -28,11 +28,57 @@ output2:save("output2");
 
     finalize(L)
 
-    create_quiver_tests("input3")
-    create_quiver_tests("output1")
-    create_quiver_tests("output2")
+    open_quiver("output1") do q
+        @test q.metadata.frequency == "month"
+        # @test q.metadata.initial_date == DateTime("2024-01-01T00:00:00")
+        @test q.metadata.number_of_dimensions == 3
+        @test q.metadata.dimensions == [:stage, :scenario, :block]
+        @test q.metadata.time_dimension == :stage
+        @test q.metadata.unit == ""
+        @test q.metadata.dimension_size == [12, 1, 1]
+        @test q.metadata.number_of_time_series == 4
+        @test q.metadata.labels == ["data_stage", "data_scenario", "data_block", "data_constant"]
 
-    delete_files(["output1"])
+        @test Quiver.goto!(q; stage = 1, scenario = 1, block = 1) ≈ [1.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 2, scenario = 1, block = 1) ≈ [2.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 3, scenario = 1, block = 1) ≈ [3.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 4, scenario = 1, block = 1) ≈ [4.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 5, scenario = 1, block = 1) ≈ [5.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 6, scenario = 1, block = 1) ≈ [6.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 7, scenario = 1, block = 1) ≈ [7.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 8, scenario = 1, block = 1) ≈ [8.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 9, scenario = 1, block = 1) ≈ [9.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 10, scenario = 1, block = 1) ≈ [10.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 11, scenario = 1, block = 1) ≈ [11.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 12, scenario = 1, block = 1) ≈ [12.0, 1.0, 1.0, 1.0]
+    end
+
+    open_quiver("output2") do q
+        @test q.metadata.frequency == "month"
+        # @test q.metadata.initial_date == DateTime("2024-01-01T00:00:00")
+        @test q.metadata.number_of_dimensions == 3
+        @test q.metadata.dimensions == [:stage, :scenario, :block]
+        @test q.metadata.time_dimension == :stage
+        @test q.metadata.unit == ""
+        @test q.metadata.dimension_size == [12, 1, 1]
+        @test q.metadata.number_of_time_series == 4
+        @test q.metadata.labels == ["data_stage", "data_scenario", "data_block", "data_constant"]
+
+        @test Quiver.goto!(q; stage = 1, scenario = 1, block = 1) ≈ [25.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 2, scenario = 1, block = 1) ≈ [26.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 3, scenario = 1, block = 1) ≈ [27.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 4, scenario = 1, block = 1) ≈ [28.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 5, scenario = 1, block = 1) ≈ [29.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 6, scenario = 1, block = 1) ≈ [30.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 7, scenario = 1, block = 1) ≈ [31.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 8, scenario = 1, block = 1) ≈ [32.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 9, scenario = 1, block = 1) ≈ [33.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 10, scenario = 1, block = 1) ≈ [34.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 11, scenario = 1, block = 1) ≈ [35.0, 1.0, 1.0, 1.0]
+        @test Quiver.goto!(q; stage = 12, scenario = 1, block = 1) ≈ [36.0, 1.0, 1.0, 1.0]
+    end
+
+    delete_files(["output1", "output2"])
 
     return nothing
 end
