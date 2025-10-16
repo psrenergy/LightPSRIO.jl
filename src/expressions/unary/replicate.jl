0687f3ef63460de53a32bdf6be982093a08e414e
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-10-16 17:26:25 -0300

    Update

--- src/expressions/unary/replicate.jl
+++ src/expressions/unary/replicate.jl
@@ -1,4 +1,4 @@
-function replicate(e1::AbstractExpression, dimension::String, times::Integer)
-    return ExpressionConcatenate(dimension, fill(e1, times))
+function replicate(e1::AbstractExpression, dimension::String, times::Number)
+    return ExpressionConcatenate(dimension, fill(e1, Int(times)))
 end
 @define_lua_function replicate
