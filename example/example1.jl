c25f94cd13ff2ef684a406d30a9dd20e487e922c
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-10-23 16:53:13 -0300

    Update

--- example/example1.jl
+++ example/example1.jl
@@ -1,7 +1,7 @@
 using LightPSRIO
 
-case_path = raw"C:\Users\rsampaio\Downloads\results"
-script_path = raw"C:\Development\PSRIO\LightPSRIO.jl\example\example.lua"
+case_path = raw"C:\Users\rsampaio\Downloads\deck_Iara_17"
+script_path = raw"C:\Development\PSRIO\LightPSRIO.jl\example\example1.lua"
 
 L = LightPSRIO.initialize([case_path])
 LightPSRIO.run_file(L, script_path)
--- example/example1.lua
+++ example/example1.lua
@@ -13,32 +13,11 @@ end
 local function tab_demand_analysis()
     local tab = Tab("Demand Analysis");
 
-    local demand = generic:load("demand"):aggregate_agents(BY_SUM(), "Total Demand"):aggregate("scenario", BY_AVERAGE());
-
-    local hydro_generation = generic:load("hydro_generation"):aggregate_agents(BY_SUM(), "Total Hydro"):aggregate("scenario", BY_AVERAGE());
-
-    local thermal_generation = generic:load("thermal_generation"):aggregate_agents(BY_SUM(), "Total Thermal"):aggregate("scenario", BY_AVERAGE());
-
-    local deficit = generic:load("deficit"):aggregate_agents(BY_SUM(), "Total Deficit"):aggregate("scenario", BY_AVERAGE());
-
-    local chart = Chart("Generation");
-    chart:add("line", demand);
-    chart:add("area_stacking", deficit, { color = "black" });
-    chart:add("area_stacking", thermal_generation, { color = "red" });
-    chart:add("area_stacking", hydro_generation, { color = "blue" });
-    tab:push(chart);
-
     return tab;
 end
 
 local function tab_hydro_analysis()
     local tab = Tab("Hydro Analysis");
-
-    local inflow = generic:load("hydro_inflow"):aggregate("scenario", BY_AVERAGE());
-
-    local chart = Chart("Inflow");
-    chart:add("line", inflow);
-    tab:push(chart);
 
     return tab;
 end
@@ -55,9 +34,4 @@ end
 
 local dashboard = Dashboard();
 dashboard:push(tab_home());
-dashboard:push(tab_cost_analysis());
-dashboard:push(tab_demand_analysis());
-dashboard:push(tab_hydro_analysis());
-dashboard:push(tab_thermal_analysis());
-dashboard:push(tab_renewable_analysis());
 dashboard:save("dashboard");
