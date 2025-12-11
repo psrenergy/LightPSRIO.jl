39795a49c00f7299ddfd928d1365792cabd26088
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-12-11 15:59:25 -0300

    Update

--- example/general_time_series.jl
+++ example/general_time_series.jl
@@ -1,8 +1,8 @@
 using LightPSRIO
 
 # path = raw"C:\Development\PSRIO\LightPSRIO.jl\data"
-path = raw"C:\Development\Papers\GeneralTimeSeriesApproximationInSDDP.jl\data"
-# path = raw"C:\Development\Papers\GeneralTimeSeriesApproximationInSDDP.jl\results"
+# path = raw"C:\Development\Papers\GeneralTimeSeriesApproximationInSDDP.jl\data"
+path = raw"C:\Development\Papers\GeneralTimeSeriesApproximationInSDDP.jl\results"
 script_path = raw"C:\Development\PSRIO\LightPSRIO.jl\example\general_time_series.lua"
 
 L = LightPSRIO.initialize([path])
