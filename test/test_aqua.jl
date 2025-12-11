b380db748550fe895c26540b85bebc60d4453f9f
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-12-11 16:03:42 -0300

    Update

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,13 +1,13 @@
-# module TestAqua
+module TestAqua
 
-# using Aqua
-# using LightPSRIO
-# using Test
+using Aqua
+using LightPSRIO
+using Test
 
-# @testset "Aqua" begin
-#     Aqua.test_ambiguities(LightPSRIO, recursive = false)
-#     Aqua.test_all(LightPSRIO, ambiguities = false)
-#     return nothing
-# end
+@testset "Aqua" begin
+    Aqua.test_ambiguities(LightPSRIO, recursive = false)
+    Aqua.test_all(LightPSRIO, ambiguities = false)
+    return nothing
+end
 
-# end
+end
