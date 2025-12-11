5b734ffa0dbbf262fdffb2c081dc5903e2dcadd4
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-12-11 16:17:19 -0300

    Update

--- format/format.jl
+++ format/format.jl
@@ -3,7 +3,8 @@ Pkg.instantiate()
 
 using JuliaFormatter
 
-if format(dirname(@__DIR__))
+if format(dirname(@__DIR__), verbose = true)
+    @info "All files are properly formatted."
     exit(0)
 else
     @error "Some files have not been formatted!"
